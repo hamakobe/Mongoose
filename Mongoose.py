48ce3e0691fd198bd6e0f71392bbe10fc639e3ca
Harmon Amakobe <hamakobe@users.noreply.github.com>
2017-07-01 19:20:44 -0400

    Writing to file
    This update has it writing the highest top speed to a log file within the AC App folder. It does not update by a predermined time nor have accurate timestamp data.

--- Mongoose.py
+++ Mongoose.py
@@ -5,6 +5,7 @@ import os
 import os.path
 import platform
 import math
+import datetime
 
 if platform.architecture()[0] == "64bit":
     sysdir=os.path.dirname(__file__)+'/shared_memory/stdlib64'
@@ -19,39 +20,40 @@ l_lapcount=0
 lapcount=0
 l_speed=0
 topspeed=0
-l_fuel=0
-fuel=0
-LTR_TO_GAL_CONVERSION = 0.26174
+sessionstarttime = datetime.datetime.now().strftime(' %b, %d, %Y %H %M %S')
+targetdir = os.path.dirname(__file__)+'/Logs/'
+targetname = "{d}Session from {t}.txt".format(d=targetdir,t=sessionstarttime)
+targetfile = open(targetname,"w")
 
 def acMain(ac_version):
 
-    global l_lapcount, l_speed, l_fuel, topspeed
+    global l_lapcount, l_speed, topspeed, targetdir
     appWindow = ac.newApp("Mongoose")
     
     ac.setSize(appWindow, 250, 200)
-
-    #ac.log(print("Hello Assetto Corsa log!")
-    #ac.console("Hello Assetto Corsa console!")
+    
+    #ac.console("The target file name is: {}".format(targetname))
     
     l_speed = ac.addLabel(appWindow, "Highest Top Speed: {}".format(topspeed));
     l_lapcount = ac.addLabel(appWindow, "Laps: 0");
-    l_fuel = ac.addLabel(appWindow, "Fuel: 0");
     ac.setPosition(l_lapcount, 3, 30)
     ac.setPosition(l_speed, 3, 60)
-    ac.setPosition(l_fuel, 3, 90)
     
     return "Lap Counter"
     
 def acUpdate(deltaT):
-    global l_lapcount, l_speed, lapcount, topspeed, l_fuel, fuel, LTR_TO_GAL_CONVERSION
+    global l_lapcount, l_speed, lapcount, topspeed, targetfile
     laps = ac.getCarState(0, acsys.CS.LapCount)
     speed = ac.getCarState(0, acsys.CS.SpeedMPH)
-    fuel = info.physics.fuel * LTR_TO_GAL_CONVERSION
-    ac.setText(l_fuel, "Current Fuel Level: {} gal".format(round(fuel,2)))
+    currenttime = 0
     
     if speed > topspeed:
         topspeed = round(speed,2)
+        currenttime = datetime.datetime.now().strftime('%H:%M:%S')
         ac.setText(l_speed, "Highest Top Speed: {} MPH".format(topspeed))
+        targetfile.write("{c} {s} MPH \n".format(c=currenttime,s=topspeed))
+        
+        
         
     if laps > lapcount:
         lapcount = laps
