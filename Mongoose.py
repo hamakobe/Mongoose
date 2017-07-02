e0cb430f780b64a3043011ea7d8ca7114284ca0f
Harmon Amakobe <hamakobe@users.noreply.github.com>
2017-07-02 12:15:54 -0400

    Updating every 5 secs
    Latest update has it writing to the file every 5 seconds. System hangs on each file write and it seems to write multiple entries per loop.

--- Mongoose.py
+++ Mongoose.py
@@ -6,6 +6,7 @@ import os.path
 import platform
 import math
 import datetime
+import time
 
 if platform.architecture()[0] == "64bit":
     sysdir=os.path.dirname(__file__)+'/shared_memory/stdlib64'
@@ -19,7 +20,6 @@ from shared_memory.sim_info import info
 l_lapcount=0
 lapcount=0
 l_speed=0
-topspeed=0
 sessionstarttime = datetime.datetime.now().strftime(' %b, %d, %Y %H %M %S')
 targetdir = os.path.dirname(__file__)+'/Logs/'
 targetname = "{d}Session from {t}.txt".format(d=targetdir,t=sessionstarttime)
@@ -27,14 +27,13 @@ targetfile = open(targetname,"w")
 
 def acMain(ac_version):
 
-    global l_lapcount, l_speed, topspeed, targetdir
+    global l_lapcount, targetdir
     appWindow = ac.newApp("Mongoose")
     
     ac.setSize(appWindow, 250, 200)
     
     #ac.console("The target file name is: {}".format(targetname))
     
-    l_speed = ac.addLabel(appWindow, "Highest Top Speed: {}".format(topspeed));
     l_lapcount = ac.addLabel(appWindow, "Laps: 0");
     ac.setPosition(l_lapcount, 3, 30)
     ac.setPosition(l_speed, 3, 60)
@@ -42,19 +41,16 @@ def acMain(ac_version):
     return "Lap Counter"
     
 def acUpdate(deltaT):
-    global l_lapcount, l_speed, lapcount, topspeed, targetfile
+    global l_lapcount, l_speed, lapcount, targetfile
     laps = ac.getCarState(0, acsys.CS.LapCount)
     speed = ac.getCarState(0, acsys.CS.SpeedMPH)
     currenttime = 0
     
-    if speed > topspeed:
-        topspeed = round(speed,2)
+    for i in range(600):
+        time.sleep(1 - time.monotonic() % 5)
         currenttime = datetime.datetime.now().strftime('%H:%M:%S')
-        ac.setText(l_speed, "Highest Top Speed: {} MPH".format(topspeed))
-        targetfile.write("{c} {s} MPH \n".format(c=currenttime,s=topspeed))
-        
-        
-        
+        targetfile.write("{c} {s} MPH \n".format(c=currenttime,s=speed))
+           
     if laps > lapcount:
         lapcount = laps
         ac.setText(l_lapcount, "Laps: {}".format(lapcount))
