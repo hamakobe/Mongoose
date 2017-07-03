b1d3286e53289771d563421ffb0e0093ec56b6d7
Harmon Amakobe <hamakobe@users.noreply.github.com>
2017-07-03 15:56:30 -0400

    Changed acUpdate to be time based
    App will write to the file every tenth of a second.
    
    I've also added comments to the code to explain methodology.
    
    Used some code by laszlo barzo from here: http://www.assettocorsa.net/forum/index.php?threads/acupdate-any-slower-options-than-deltat.5727/

--- Mongoose.py
+++ Mongoose.py
@@ -6,51 +6,78 @@ import os.path
 import platform
 import math
 import datetime
-import time
 
+#Picking between 64bit or 32bit libraries for the physics modules (fuel etc.)
 if platform.architecture()[0] == "64bit":
     sysdir=os.path.dirname(__file__)+'/shared_memory/stdlib64'
 else:
     sysdir=os.path.dirname(__file__)+'/shared_memory/stdlib'
 sys.path.insert(0, sysdir)
 os.environ['PATH'] = os.environ['PATH'] + ";."
- 
+
+#Importing the info module, which inclued fuel info 
 from shared_memory.sim_info import info
 
+#Global variables
 l_lapcount=0
 lapcount=0
 l_speed=0
+tick=0
+
+#Output file name and directory definition
 sessionstarttime = datetime.datetime.now().strftime(' %b, %d, %Y %H %M %S')
 targetdir = os.path.dirname(__file__)+'/Logs/'
 targetname = "{d}Session from {t}.txt".format(d=targetdir,t=sessionstarttime)
 targetfile = open(targetname,"w")
 
+#Main Assetto Corsa function, builds the App Window and the labels associated with it
 def acMain(ac_version):
-
-    global l_lapcount, targetdir
-    appWindow = ac.newApp("Mongoose")
+    global l_lapcount, targetdir, path, tick
     
+    tick=ticker() #set the global variable to be a ticker, see the class below
+    
+    appWindow = ac.newApp("Mongoose") 
     ac.setSize(appWindow, 250, 200)
     
-    #ac.console("The target file name is: {}".format(targetname))
-    
-    l_lapcount = ac.addLabel(appWindow, "Laps: 0");
+    l_lapcount = ac.addLabel(appWindow, "Laps: {}".format(0));
     ac.setPosition(l_lapcount, 3, 30)
-    ac.setPosition(l_speed, 3, 60)
     
-    return "Lap Counter"
+    return "Mongoose"
     
+#Main update function for Assetto Corsa, it runs the enclosed code every DeltaT - I think DeltaT = 1/60 of a second
 def acUpdate(deltaT):
-    global l_lapcount, l_speed, lapcount, targetfile
+    global l_lapcount, l_speed, lapcount, targetfile, tick
+
+    if tick.tack(deltaT):  #does not bother CPU with unnecessary updates, basically exists the update function call if time is less than value specified in ticker()
+        return
+     
     laps = ac.getCarState(0, acsys.CS.LapCount)
     speed = ac.getCarState(0, acsys.CS.SpeedMPH)
-    currenttime = 0
+    currenttime = datetime.datetime.now().strftime('%H:%M:%S.%f')[:-4] #gets the current system clock down to the hundredth of a second (if [-4])
     
-    for i in range(600):
-        time.sleep(1 - time.monotonic() % 5)
-        currenttime = datetime.datetime.now().strftime('%H:%M:%S')
-        targetfile.write("{c} {s} MPH \n".format(c=currenttime,s=speed))
+    targetfile.write("{c} {s} MPH \n".format(c=currenttime,s=round(speed,2)))
            
     if laps > lapcount:
         lapcount = laps
-        ac.setText(l_lapcount, "Laps: {}".format(lapcount))
+        ac.setText(l_lapcount, "Laps: {}".format(lapcount)) #updates the label in the App Window defined on acMain
+        
+#-----------------------
+# ticker function, to determine update rate
+#--------------------
+class ticker:
+    def __init__(self):
+        self.ticktimer = 0.0
+        self.ticktime = 0.0
+        self.tickrate = 0.1 #update rate in seconds
+   
+    def tack(self,deltaT):
+        self.ticktime += deltaT
+        self.ticktimer += deltaT
+        if self.ticktime >= self.tickrate:
+            self.ticktime = self.ticktime % self.tickrate
+            return False
+        else:
+            return True
+   
+    def debuginfo(self):
+        return "ticktimer: %s ticktime: %s tickrate: %s" % (self.ticktimer, self.ticktime, self.tickrate)
